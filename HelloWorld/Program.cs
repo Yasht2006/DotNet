e1f4a5046e83e5307f5a0d7b010b617a0cebe18e
trainee1 <trainee1@tmspl.com>
2025-07-08 17:21:37 +0530

    Test Changes Commit

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+class Hello
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World.!");
+        Console.WriteLine(args.Length);
+        //Console.WriteLine(args[0]);
+        Console.ReadKey();
+
+        //int grade = 90;
+        //int* ptr = &grade;
+
+        //Console.WriteLine("Original Grade: " + grade);
+        //Console.WriteLine("Memory Address: " + (ulong)ptr);
+
+        //*ptr = 95; // Modifying value using pointer
+        //Console.WriteLine("Updated Grade: " + grade);
+    }
+}
